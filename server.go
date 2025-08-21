d80313a000071d065a0be9f85dcc0428b5a3551b
Matthew Winter <33818+wintermi@users.noreply.github.com>
2025-08-21 12:12:40 +1000

    Increase the timeout to 1 hour

--- server.go
+++ server.go
@@ -42,8 +42,8 @@ func NewServer(addr, staticDir string) (*Server, error) {
 	server := &http.Server{
 		Addr:         addr,
 		Handler:      r,
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  3600 * time.Second,
+		WriteTimeout: 3600 * time.Second,
 		IdleTimeout:  60 * time.Second,
 	}
 
